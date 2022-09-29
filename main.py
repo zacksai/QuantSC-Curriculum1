f7ad3dd203a6f140fbbe38099feedb6fd97a2e22
Zack Sai <zackjsai@gmail.com>
2022-09-28 21:23:31 -0700

    configured interpreter and notebook

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
+# author: Zack Sai
+# import statements
+import os
+import time
+import hmac
+from dotenv import load_dotenv
+import requests
+import pandas as pd
+import urllib
+import numpy as np
+load_dotenv('.env')
+os.environ.get('test')
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
